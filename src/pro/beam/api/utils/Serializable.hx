1112fce0efc5b6f3540bf0b149302679ee685109
DragOnGamer626 <draygera@gmail.com>
2016-06-26 20:05:51 -0500

    Starting Work on Serialization Code

---
+++ src/pro/beam/api/utils/Serializable.hx
@@ -0,0 +1,11 @@
+package pro.beam.api.utils;
+
+/**
+ * @author DragOnGamer626
+ */
+interface Serializable 
+{
+	public function add(name : String, value : Dynamic) : Serializable;
+	
+	public function build() : Void;
+}
