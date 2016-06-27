692a122cbeb44e8440cbf1a076bf0fd4d0d4810e
DragOnGamer626 <draygera@gmail.com>
2016-06-27 15:57:47 -0500

    Created Audience Enum

---
+++ src/pro/beam/api/resources/enums/Audience.hx
@@ -0,0 +1,12 @@
+package pro.beam.api.resources.enums;
+
+/**
+ * ...
+ * @author DragOnGamer626
+ */
+@:enum abstract Audience(String) 
+{
+	var FAMILY = "Family";
+	var TEEN = "Teen";
+	var ADULT = "18+";
+}
