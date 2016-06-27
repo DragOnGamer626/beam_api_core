472c929c2d018e48415f5ab109b0e087439dc94b
DragOnGamer626 <draygera@gmail.com>
2016-06-27 16:52:15 -0500

    Forgot Some Functions Whoops

--- src/pro/beam/api/resources/Notification.hx
+++ src/pro/beam/api/resources/Notification.hx
@@ -8,6 +8,7 @@ class Notification extends AbstractResource
 {
 	public var createdAt(default, set) : Date;
 	public var readAt(default, set) : Date;
+	public var type(default, set) : String;
 	
 	var data : Array<Dynamic>;
 	
@@ -15,6 +16,21 @@ class Notification extends AbstractResource
 	{
 		super(name);
 		this.data = new Array<Dynamic>();
+	}
+	
+	function set_createdAt(createdAt)
+	{
+		return this.createdAt = createdAt;
+	}
+	
+	function set_readAt(readAt)
+	{
+		return this.readAt = readAt;
+	}
+	
+	function set_type(type)
+	{
+		return this.type = type;
 	}
 	
 	public function addData(data) : Notification
