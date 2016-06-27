f7e21d8e8622ff8a6832b6e92185bc1939b0f4f6
DragOnGamer626 <draygera@gmail.com>
2016-06-26 22:56:06 -0500

    Created Message Class with Basic Params

---
+++ src/pro/beam/api/resources/Message.hx
@@ -0,0 +1,28 @@
+package pro.beam.api.resources;
+
+/**
+ * Message
+ * @author DragOnGamer626
+ */
+class Message extends AbstractResource 
+{
+	public var data(default, set) : String;
+	public var type(default, set) : String;
+	
+	public function new(name:String, ?data, ?type) 
+	{
+		super(name);
+		set_data(data);
+		set_type(type);
+	}
+	
+	function set_data(data)
+	{
+		return this.data = data;
+	}
+	
+	function set_type(type)
+	{
+		return this.type = type;
+	}
+}
