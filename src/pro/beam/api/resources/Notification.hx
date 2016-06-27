c03ef871a49f14c52edda40c01aecd525beae84e
DragOnGamer626 <draygera@gmail.com>
2016-06-27 12:13:27 -0500

    Created Notification Class

---
+++ src/pro/beam/api/resources/Notification.hx
@@ -0,0 +1,16 @@
+package pro.beam.api.resources;
+
+/**
+ * Notification
+ * @author DragOnGamer626
+ */
+class Notification extends AbstractResource 
+{
+
+	public function new(name:String)
+	{
+		super(name);
+		
+	}
+	
+}
