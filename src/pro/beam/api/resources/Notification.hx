69a1b325f4b6c3d071280942b77f2cdca6ebcff5
DragOnGamer626 <draygera@gmail.com>
2016-06-27 16:48:28 -0500

    Created Notification class. Needs Work

--- src/pro/beam/api/resources/Notification.hx
+++ src/pro/beam/api/resources/Notification.hx
@@ -6,11 +6,26 @@ package pro.beam.api.resources;
  */
 class Notification extends AbstractResource 
 {
-
+	public var createdAt(default, set) : Date;
+	public var readAt(default, set) : Date;
+	
+	var data : Array<Dynamic>;
+	
 	public function new(name:String)
 	{
 		super(name);
-		
+		this.data = new Array<Dynamic>();
+	}
+	
+	public function addData(data) : Notification
+	{
+		this.data.push(data);
+		return this;
+	}
+	
+	public function getData() : Array<Dynamic>
+	{
+		return data;
 	}
 	
 }
