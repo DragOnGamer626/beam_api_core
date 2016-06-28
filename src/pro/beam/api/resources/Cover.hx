ce3937f4edc5c555f37e1edb0a93d7c95ed686e3
DragOnGamer626 <draygera@gmail.com>
2016-06-27 22:15:20 -0500

    Added Cover Class

---
+++ src/pro/beam/api/resources/Cover.hx
@@ -0,0 +1,16 @@
+package pro.beam.api.resources;
+
+/**
+ * Cover
+ * @author DragOnGamer626
+ */
+class Cover extends AbstractResource 
+{
+	
+	public function new(?name:String) 
+	{
+		super(name);
+		
+	}
+	
+}
