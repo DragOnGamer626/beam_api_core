8d6973b1662ebb9c4035a25f92c695be76087c73
DragOnGamer626 <draygera@gmail.com>
2016-06-27 22:35:27 -0500

    Bookoo Channel Work

--- src/pro/beam/api/resources/Channel.hx
+++ src/pro/beam/api/resources/Channel.hx
@@ -11,12 +11,18 @@ class Channel extends AbstractResource
 	public var badgeID(default, set) : String;
 	// Cache param here
 	public var cover(default, set) : Cover;
+	public var coverID(default, set) : String;
+	public var deletedAt(default, set) : Date;
+	public var description(default, set) : String;
+	public var featured(default, set) : Bool = false;
+	public var ftl(default, set) : Int;
 	public var online(default, set) : Bool = false;
 	public var partnered(default, set) : Bool = false;
 	public var interactive(default, set) : Bool = false;
 	public var followers(default, set) : Int;
 	public var subscribers(default, set) : Int;
 	public var title(default, set) : String;
+	public var streamType(default, set) : String;
 	public var game(default, set) : String;
 	
 	public function new(?name:String, ?id) 
@@ -43,6 +49,31 @@ class Channel extends AbstractResource
 	function set_cover(cover)
 	{
 		return this.cover = cover;
+	}
+	
+	function set_coverID(coverID)
+	{
+		return this.coverID = coverID;
+	}
+	
+	function set_deletedAt(deletedAt)
+	{
+		return this.deletedAt = deletedAt;
+	}
+	
+	function set_description(description)
+	{
+		return this.description = description;
+	}
+	
+	function set_featured(featured)
+	{
+		return this.featured = featured;
+	}
+	
+	function set_ftl(ftl)
+	{
+		return this.ftl = ftl;
 	}
 	
 	function set_online(online)
@@ -75,8 +106,14 @@ class Channel extends AbstractResource
 		return this.title = title;
 	}
 	
+	function set_streamType(streamType)
+	{
+		return this.streamType = streamType;
+	}
+	
 	function set_game(game)
 	{
 		return this.game = game;
 	}
+	
 }
